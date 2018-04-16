9f4babed073af6b083bd543410ce17b4d1bbe7b8
Steven Rondeau <steve.rondeau@sentinelspc.com>
2018-04-16 13:53:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,43 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+
+var string = "hello"
+//ring.toUpperCase() === string // true
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.")  {
+    return "I love you, too.";
+ } else if (string.toLowerCase() === "hello") {
+     return "I can't hear you!";
+     return "YES INDEED!";
+        }
+   console.log(string)
+}
+
+/*if (time < 10) {
+    greeting = "Good morning";
+} else if (time < 20) {
+    greeting = "Good day";
+} else {
+    greeting = "Good evening";
+}
+*/
+
+
+
+
+//var lowercase = 'hello!'*/
