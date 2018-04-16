f56313cc1e6a2033184681069c59aeba90758813
Steven Rondeau <steve.rondeau@sentinelspc.com>
2018-04-16 14:09:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function sayHiToGrandma(string) {
  } else if (string.toLowerCase() === string) {
      return "I can't hear you!";
  } else if (string.toUpperCase() === string) {
-     return "YES INDEED!";    
+     return "YES INDEED!";
         }
      return "Hey!"
    }
